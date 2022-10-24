b6d796356cc86711dcde574dbc9a4f3fb18c8ad2
MuddanaSatyaPavani <111487125+MuddanaSatyaPavani@users.noreply.github.com>
2022-10-24 16:55:13 +0530

    #include<stdio.h> int main() {     int n,i,fact,rem;     scanf("%d",&n);     int sum=0,temp=n;     while(n)     {         i=1,fact=1;         rem=n%10;         while(i<=rem)         {             fact=fact*i;             i++;         }         sum=sum+fact;         n=n/10;     }     if(sum==temp)     {         printf("The number %d is a strong number ",temp);     }     else     {         printf("The number %d is not a strong number ",temp);     } }

---
+++ Strong_Number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,fact,rem;
+    scanf("%d",&n);
+    int sum=0,temp=n;
+    while(n)
+    {
+        i=1,fact=1;
+        rem=n%10;
+        while(i<=rem)
+        {
+            fact=fact*i;
+            i++;
+        }
+        sum=sum+fact;
+        n=n/10;
+    }
+    if(sum==temp)
+    {
+        printf("The number %d is a strong number
+",temp);
+    }
+    else
+    {
+        printf("The number %d is not a strong number
+",temp);
+    }
+}
